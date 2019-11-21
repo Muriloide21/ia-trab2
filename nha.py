dccd1bd58666f417b69175253664ed6d0874c30b
Murilo Schirmer <muriloschirmer16@gmail.com>
2019-11-20 22:11:55 -0300

    Centroide funcionando e corpo do trabalho surgindo

--- nha.py
+++ nha.py
@@ -15,8 +15,19 @@ from sklearn.utils.multiclass import unique_labels
 from sklearn.metrics import euclidean_distances
 from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 from sklearn.preprocessing import KBinsDiscretizer
+import statistics as st
+
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 class ZeroR(BaseEstimator, ClassifierMixin):
+
+    def get_name(self):
+        return "ZeroR"
+
     def __init__(self, demo_param='demo'):
         self.demo_param = demo_param
 
@@ -36,6 +47,9 @@ class ZeroR(BaseEstimator, ClassifierMixin):
 
 class OneR(BaseEstimator, ClassifierMixin):
      
+    def get_name(self):
+        return "OneR"
+
     def __init__(self, demo_param='demo'):
         self.demo_param = demo_param
 
@@ -65,33 +79,60 @@ class OneR(BaseEstimator, ClassifierMixin):
                 maior_soma = soma
                 definitivo = tmp
                 self.index_feature_definitiva = index
-        print("'Tabelinhas': ",definitivo)
-        #print(X.T[self.index_feature_definitiva])
         values_feature = unique_labels(X.T[self.index_feature_definitiva])
-        print("Valores possíveis da feature: ", values_feature)
-        #self.regras = [np.argmax(definitivo[i]) for i in X.T[self.index_feature_definitiva].astype(np.int64)]
         self.regras = [np.argmax(definitivo[i]) for i in values_feature.astype(np.int64)]
-        print("self.regras: ", self.regras)
+        #Caso as regras baseadas em uma feature não abranjam todas as classes, a classe default é 0
+        while(len(self.regras) < len(self.classes_)):
+            self.regras.append(0)
 		# Return the classifier
         return self
 
     def predict(self, X):
-        # self.feature = unique_labels(X.T[self.index_feature_definitiva])
-        # self.disc = KBinsDiscretizer(n_bins = len(self.feature), encode='ordinal', strategy = 'quantile')
-        # X = self.disc.fit_transform(X)
         X = self.disc.fit_transform(X)
         feature = X.T[self.index_feature_definitiva]
-        #print(self.index_feature_definitiva)
-        print(X.T[3])
-        return [self.regras[i] for i in feature.astype(np.int64)]
+        predict = [self.regras[i] for i in feature.astype(np.int64)]
+        return predict
 
-iris = datasets.load_iris()
-X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.4, random_state=0)
-nn = OneR()
-nn.fit(X_train, y_train) 
-print(nn.predict(X_test))
-# print(nn.score(X_test, y_test))
-# print(nn.get_params())
-# scores = cross_val_score(nn, iris.data, iris.target, cv=5)
-# print(scores)
 
+
+class Centroide(BaseEstimator, ClassifierMixin):
+    def get_name(self):
+        return "Centroide"
+
+    def __init__(self, demo_param='demo'):
+        self.demo_param = demo_param
+
+    def fit(self, X, y):
+        self.classes = unique_labels(y)
+        self.centroids = []
+        for c in self.classes:
+            indexes = np.where(c == y)
+            centroid = []
+            for feature in X.T:
+                mean_values_feature = st.mean([feature[k] for k in indexes[0]])
+                centroid.append(mean_values_feature)
+            self.centroids.append(centroid)
+        #print(self.centroids)
+
+    def predict(self, X):
+        return([self.classes[np.argmin([max(max(euclidean_distances([x], [centroide]))) for centroide in self.centroids])] for x in X])
+            #print(x)
+
+# iris = datasets.load_iris()
+# knn = KNeighborsClassifier()
+# grade = {'n_neighbors': [1, 3, 5]}
+# gs = GridSearchCV(estimator=knn, param_grid = grade, scoring='accuracy', n_jobs=-1, cv = 4)
+# scores = cross_val_score(gs, iris.data, iris.target, scoring='accuracy', cv = 10)
+# print ('CV Accuracy: %.3f +/- %.3f' % (np.mean(scores), st.stdev(scores)))
+# print (scores)
+
+datasets = [datasets.load_iris(), datasets.load_digits(), datasets.load_wine(), datasets.load_breast_cancer()]
+classificadores = [ZeroR(), OneR(), Centroide()]
+for d in datasets:
+    base = d
+    for c in classificadores:
+        scores = cross_val_score(c, base.data, base.target, cv=10)
+        print("Classificador: "+c.get_name())
+        print('CV Accuracy: %.5f +/- %.5f' % (st.mean(scores), st.stdev(scores)))
+        #print (scores)
+    print("--------------------------------------")

fa9d461ba185be3f67d89a13000063842352cae4
Murilo Schirmer <muriloschirmer16@gmail.com>
2019-11-22 20:28:26 -0300

    Já está gerando as tabelas, falta os boxplots

--- nha.py
+++ nha.py
@@ -16,6 +16,9 @@ from sklearn.metrics import euclidean_distances
 from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 from sklearn.preprocessing import KBinsDiscretizer
 import statistics as st
+import seaborn as sns
+import matplotlib.pyplot as plt
+from tabulate import tabulate
 
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
@@ -118,28 +121,79 @@ class Centroide(BaseEstimator, ClassifierMixin):
         return([self.classes[np.argmin([max(max(euclidean_distances([x], [centroide]))) for centroide in self.centroids])] for x in X])
             #print(x)
 
+def example1():
+    mydata=[1,2,3,4,5,6,12]
+    sns.boxplot(y=mydata) # Also accepts numpy arrays
+    plt.show()
+
+def example2():
+    df = sns.load_dataset('iris')
+    #returns a DataFrame object. This dataset has 150 examples.
+    #print(df)
+    # Make boxplot for each group
+    sns.boxplot( data=df.loc[:,:] )
+    # loc[:,:] means all lines and all columns
+    plt.show()
 
 datasets = [datasets.load_iris(), datasets.load_digits(), datasets.load_wine(), datasets.load_breast_cancer()]
 classificadores = [ZeroR(), OneR(), Centroide(), GaussianNB()]
 classificadores2 = [KNeighborsClassifier(), DecisionTreeClassifier(), MLPClassifier(), RandomForestClassifier()]
 hiperparametros = [{'n_neighbors': [1, 3, 5]}, {'max_depth' : [None, 3, 5, 10]}, {'max_iter' : [50, 100, 200], 'hidden_layer_sizes' : [(15,)]}, {'n_estimators' : [10, 20, 50, 100]}]
 datasets_names = ["Iris", "Digits", "Wine", "Breast Cancer"]
+classificadores_nomes = ["ZeroR", "OneR", "Centroide", "Naive Bayes Gaussiano"]
+classificadores2_nomes = ["Knn", "Árvore de Decisão", "Redes Neurais", "Florestas de Árvores"]
+
 for i,d in enumerate(datasets):
     base = d
-    print(datasets_names[i])
-    # for c in classificadores:
-    #     scores = cross_val_score(c, base.data, base.target, cv=10)
-    #     print("Classificador: "+c.get_name())
-    #     print('CV Accuracy: %.5f +/- %.5f' % (st.mean(scores), st.stdev(scores)))
-    #     #print (scores)
-    # print("--------------------------------------")
+    mean_accuracies = []
+    standard_deviations = []
+    mean_accuracies2 = []
+    standard_deviations2 = []
+    dataset_name = datasets_names[i]
+
+    print(dataset_name)
+    for ind,c in enumerate(classificadores):
+        scores = cross_val_score(c, base.data, base.target, cv=10)
+        print("Classificador: ", classificadores_nomes[ind])
+        print('CV Accuracy: %.5f +/- %.5f' % (st.mean(scores), st.stdev(scores)))
+        mean_accuracies.append(st.mean(scores))
+        standard_deviations.append(st.stdev(scores))
+        #print (scores)
+    print("--------------------------------------")
     for index,m in enumerate(classificadores2):
         #print(index)
         #print(hiperparametros[index])
         grade = hiperparametros[index]
         gs = GridSearchCV(estimator=m, param_grid = grade, scoring='accuracy', cv = 4)
         gs.fit(base.data, base.target)
+        print("Classificador: ", classificadores2_nomes[index])
         print(gs.best_params_)
         scores = cross_val_score(gs, base.data, base.target, scoring='accuracy', cv = 10)
         print ('CV Accuracy: %.5f +/- %.5f' % (np.mean(scores), st.stdev(scores)))
+        mean_accuracies2.append(st.mean(scores))
+        standard_deviations2.append(st.stdev(scores))
         #print (scores)
+
+    table = []
+    for j in range(len(classificadores)):
+        table.append([classificadores_nomes[j], mean_accuracies[j], standard_deviations[j]]
+    )
+
+    table2 = []
+    for k in range(len(classificadores2)):
+        table2.append([classificadores2_nomes[k], mean_accuracies2[k], standard_deviations2[k]]
+    )
+
+    header = ['Classificador','Média Acurácias','Desvio Padrão']
+    file = open(dataset_name+".txt", 'w')
+    file.write(tabulate(table,header,stralign="center",numalign="center",tablefmt="latex"))
+    file.close()
+
+    file = open(dataset_name+"2.txt", 'w')
+    file.write(tabulate(table2,header,stralign="center",numalign="center",tablefmt="latex"))
+    file.close()
+        # example1()
+        # example2()  
+
+
+
